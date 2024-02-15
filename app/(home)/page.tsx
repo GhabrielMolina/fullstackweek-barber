1dfb6f75ada2db8dc69c4634627485415cf18bc3
Ghabriel <ghabrielmolina@hotmail.com>
2024-02-15 10:55:27 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,13 +5,26 @@
 // "utils": "@/app/_lib/utils"
 
 
-import Image from "next/image";
+//npm i date-fns
+//https://date-fns.org/v3.3.1/docs/format
+import { format } from 'date-fns'
 import Header from "../_components/header";
+import { ptBR } from 'date-fns/locale/pt-BR';
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className='px-5 pt-5'>
+        <h2 className='text-xl font-bold'>Olá, Miguel</h2>
+        <p className='capitalize text-sm'>
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
     </div>
   );
 }
