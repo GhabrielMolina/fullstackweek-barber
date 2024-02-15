5c986ebb323c24f12b115f3c4e3746d973680ab6
Ghabriel <ghabrielmolina@hotmail.com>
2024-02-15 11:31:07 -0300

    feat: add initial booking _component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,6 +11,7 @@ import { format } from 'date-fns'
 import Header from "../_components/header";
 import { ptBR } from 'date-fns/locale/pt-BR';
 import Search from './_components/search';
+import BookingItem from '../_components/booking-item';
 
 export default function Home() {
   return (
@@ -28,6 +29,11 @@ export default function Home() {
 
       <div className='px-5 mt-6'>
         <Search />
+      </div>
+
+      <div className='px-5 mt-6'>
+        <h2 className='text-xs mb-3 uppercase text-gray-400 font-bold'>Agendamentos</h2>
+          <BookingItem />
       </div>
     </div>
   );
